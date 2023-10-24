3693dc0132dd5bad3ae35efb2355005354963af7
Kirill Gorbenko <kirilg.ex@gmail.com>
2023-10-24 09:36:04 +0400

    fix: fraction digits

--- Quixpenses.App/DatabaseAccess/SeedingData/SeedingCurrencies.cs
+++ Quixpenses.App/DatabaseAccess/SeedingData/SeedingCurrencies.cs
@@ -8,7 +8,7 @@ public static class SeedingCurrencies
     public static void SeedCurrencies(this ModelBuilder builder)
     {
         builder.Entity<DbCurrency>().HasData(
-            new DbCurrency { Id = "USD", FractionDigits = 100 },
-            new DbCurrency { Id = "EUR", FractionDigits = 100 });
+            new DbCurrency { Id = "USD", FractionDigits = 2 },
+            new DbCurrency { Id = "EUR", FractionDigits = 2 });
     }
 }
